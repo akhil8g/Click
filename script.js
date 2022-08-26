39c402506810123e9c78cff32403cd6d15be4137
akhil8g <89475767+akhil8g@users.noreply.github.com>
2022-08-26 22:24:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let counter=//localStorage.getItem('counter') || 0;
+document.querySelector("#n").innerHTML=counter;
+function updateClicks() {
+    counter++;
+    document.querySelector("#n").innerHTML=counter;
+    localStorage.setItem('counter',counter);
+}
+
+function clickv() {
+    document.querySelector("#clicks").style.visibility="visible";
+}
